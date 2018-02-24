0c8ba8a13318d3f8f419a187d68600d8829104f3
Furqan45 <you@example.com>
2018-02-24 10:23:01 +0500

    Intial commit

---
+++ index.js
@@ -0,0 +1,118 @@
+////Task No 1////
+// alert("Mian Muhammad furqan");
+/////Task No 2///////
+// var username =("furqan")
+// var myName = username;0
+// alert(username);
+////Task No 3/////
+// var message;
+// message="hello world"
+// alert(message);
+////Task No 4/////
+// var name =prompt("enter your name");
+// var age =prompt("enter your age");
+// var num =prompt("enter your number");
+// alert ("welcome"+" "+name+" "+"you're"+" "+age+" "+"years old");
+/////Task No 5///
+// var js =["p","I","z","z","a"];
+// for(i=0; i<5 ; i++ )
+// {alert(js);}
+/////Task No 6/////
+// var email ="furqanfasal@gmail.com";
+//  alert("your email is"+""+email);
+/////Task No 7/////
+// var age ="14";
+//  alert("I Am"+""+age+"year old");
+////Task No 8/////
+//  var time ="21";
+//  alert("you have visted"+""+time+"in shop");
+////Task No 9//////
+// var birth="1997"
+// alert("my"+" "+"Birth"+" "+"year"+" "+"is"+" "+birth);
+// ///Task No 10////
+// var visitorname ="Furqan"
+// var producttitle="XYZ Clothing store"
+// var quantity ="5 T-shirt(s)"
+// alert(visitorname+" "+"ordered"+""+quantity+""+"on"+""+producttitle);
+/////Task No 11////
+// var ticket =600;
+//  alert("price of movie ticket is "+ticket);
+//  var buy= prompt ("how many ticket you want to buy");
+//  final = ticket + buy;
+//  alert("price of"+buy+"ticket is"+final);
+///////Task NO 12////
+// var c =prompt("enter temperature in celcius");
+
+// var f;
+
+// f =(c*9/5)+32;
+// document.write("the temp is fahrenhiet is"+f+"'f")
+// c =(f-32)* 5/9;
+// document.write("<br>the temp is celcius is" +c+"'c")
+/////Task No 13///////
+// var number;
+// document.write("your value after decleration"+number)
+// number=3;
+// document.write("<br> intial value"+number);
+// number=++number;
+// document.write("<BR>value after increament is"+number);
+// number =number + 7;
+// document.write("<br>value after addition is"+number);
+// number=number + 5-6;
+// document.write("<br>value after decrement is"+number);
+// number =number + 10 / 2 -14;
+// document.write("<br>The remender  is"+number);
+///////Task No 14////////
+//  var a = +prompt("Enter Value");
+//  if(a>=80 && a<=100){
+//      alert("A")
+//  }
+//  else if(a>=70 && a<80){
+//      alert("B");
+//  }
+//  else if(a>=60 && a<70){
+//      alert("C");
+//  }
+//  else if(a>=50 && a<60){
+//      alert("D");
+//  }
+//  else if(a>=40 && a<50){
+//      alert("E");
+//  }
+//  else if(a>=0 && a<40){
+//      alert("F");
+//  }
+//  else{
+//      alert("Please insert appropriate number");
+//  }
+//////task no 15//////
+// var num1 = +prompt("enter a num");
+// var num2 = +prompt("enter second num");
+// var add = num1+num2;
+// var mul = num1*num2;
+// var div = num1/num2;
+// var sub = num1-num2;
+// var remind = num1%num2
+
+
+// alert("sum of " + num1 + " and " + num2 + " is "+ add);
+// alert("result of multiplying " + num1 + " and " + num2 + " is "+ mul);
+// alert("result of dividing " + num1 + " and " + num2 + " is "+ div);
+// alert("result of subtraction" + num1 + " and " + num2 + " is "+ sub);
+// alert("reminder of " + num1 + " and " + num2 + " is "+ remind);
+/////////Task no 16////////
+// var num = +prompt("enter value");
+// for  (var i=1; i<20; i++ ) {console.log(num+"x"+i+"="+(num*1))};
+/////////Task no 17/////
+// alert("welcome to js land../happy coding");
+
+
+
+
+
+
+
+
+
+
+
